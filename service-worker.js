73a8a10eb4b08a353a59524b8c7771dd2241925f
savitha <savitha.raghavendran@2359media.com>
2020-03-15 08:46:36 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/savitha18/Vadirajaru/precache-manifest.c82c390abe905554dc7a529062e9b435.js"
+  "/savitha18/Vadirajaru.git/precache-manifest.7c3fa9cd5b780bf375dcff06ac81370c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/savitha18/Vadirajaru/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/savitha18/Vadirajaru.git/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
